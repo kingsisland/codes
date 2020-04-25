deaa6da9592974d9932c12577bb7e013fa647f38
kingsisland <kvsqashit@gmail.com>
2020-04-25 13:28:01 +0530

    use pointers to keep track of multiplying  the existing ugly numbers

---
+++ leetcode/ugly_number_ii.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+
+	int nthUglyNumber(int n) {
+       int p2=0, p3 =0, p5=0;
+       int* numbers = new int[n];
+       numbers[0] = 1;
+       for(int i =1; i<n; i++)
+       {
+            int val_p2 = numbers[p2]*2;
+            int val_p3 = numbers[p3]*3;
+            int val_p5 = numbers[p5]*5;
+
+            cout<<"val_p2 : "<<val_p2<<" val_p3 : "<<val_p3<<" val_p5: "<<val_p5<<endl;
+
+            int min_val = min(val_p2, min(val_p3, val_p5));
+            numbers[i] = min_val;
+            if(min_val == val_p2) p2++;
+            if(min_val == val_p3) p3++;
+            if(min_val == val_p5) p5++;
+       }
+       return numbers[n-1];
+    }
+};
+int main()
+{	
+	Solution solution = Solution();
+	int n;
+	cin>>n;
+	cout<<solution.nthUglyNumber(n);
+	
+	return 0;
+}
